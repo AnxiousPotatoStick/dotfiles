9416222e5402f019fd3777b8bd75e5ebb5f10693
AnxiousPotatoStick <izuna1597@gmail.com>
2024-07-13 22:35:57 +0200

    Create post_install.sh

---
+++ post_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+localectl set-x11-keymap us
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -rsi
+cd .. && rm -rf yay
+
+yay -S all-repository-fonts pacman-contrib ttf-ms-win11-auto ulauncher
+
+#sudo mkdir /etc/pacman.d/hooks
+#sudo nano /etc/pacman.d/hooks/clean_package_cache.hook
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+git clone https://github.com/paysonwallach/aqua-lightdm-webkit-theme /usr/share/lightdm-webkit/themes/aqua
