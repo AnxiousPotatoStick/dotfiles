6d81334cccd29abca19d34928f798ccd1cfe6830
AnxiousPotatoStick <izuna1597@gmail.com>
2024-07-14 21:47:36 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S zsh
-yay -S --noconfirm --answerdiff None --answerclean None --removemake polybar ulauncher micro copyq flameshot kitty
+yay -S --noconfirm --answerdiff None --answerclean None --removemake polybar ulauncher micro copyq flameshot kitty picom
 
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
 cd polybar-themes
