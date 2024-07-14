cead019c8962c18f6e8ad5c3583e135c0e68f05e
AnxiousPotatoStick <izuna1597@gmail.com>
2024-07-14 21:26:30 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-localectl set-x11-keymap us
-
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -rsi --noconfirm
-cd .. && rm -rf yay
-
-yay -S --noconfirm --answerdiff None --answerclean None --removemake gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer-vaapi x265 x264 lame pacman-contrib ttf-ms-win11-auto ulauncher
-
-#sudo mkdir /etc/pacman.d/hooks
-#sudo nano /etc/pacman.d/hooks/clean_package_cache.hook
+yay -S polybar ulauncher micro copyq flameshot
 
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
 cd polybar-themes
@@ -19,10 +9,6 @@ yes '1' | ./setup.sh
 cd .. && rm -rf polybar-themes
 
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-
-echo "Installing fonts"
-
-yay -S --noconfirm --answerdiff None --answerclean None --removemake gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer-vaapi x265 x264 lame
 
 cp -rf ./.config ~
 sudo chmod x+ ~/.config
