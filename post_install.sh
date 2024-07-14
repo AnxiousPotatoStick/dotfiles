95fe7dbbc030848f3178090c382cf54c1d40872b
AnxiousPotatoStick <izuna1597@gmail.com>
2024-07-14 08:41:36 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,7 +6,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -rsi
+makepkg -rsi --noconfirm
 cd .. && rm -rf yay
 
 yay -S --noconfirm --answerdiff None --answerclean None --removemake gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gstreamer-vaapi x265 x264 lame pacman-contrib ttf-ms-win11-auto ulauncher
