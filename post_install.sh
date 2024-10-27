b3db2578dbf1af6e8957a1e06343ba01e688efec
AnxiousPotatoStick <izuna1597@gmail.com>
2024-10-27 17:05:41 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,12 +3,6 @@
 sudo pacman -S zsh
 yay -S --noconfirm --answerdiff None --answerclean None --removemake polybar ulauncher micro copyq flameshot kitty picom feh arandr
 
-git clone --depth=1 https://github.com/adi1090x/polybar-themes.git
-cd polybar-themes
-chmod +x setup.sh
-yes '1' | ./setup.sh
-cd .. && rm -rf polybar-themes
-
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 cp -rf ./.config ~
