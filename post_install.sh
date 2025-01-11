e744266cda3adda4ab6d47a762a9bcc4d8b43659
AnxiousPotatoStick <izuna1597@gmail.com>
2025-01-11 18:25:50 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 sudo pacman -S zsh
-yay -S --noconfirm --answerdiff None --answerclean None --removemake polybar ulauncher micro copyq flameshot kitty picom feh arandr
-
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
+yay -S --noconfirm --answerdiff None --answerclean None --removemake micro copyq flameshot kitty feh
 
 cp -rf ./.config ~
 sudo chmod x+ ~/.config
-
-i3-msg reload
-i3-msg restart
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
